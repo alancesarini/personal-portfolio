8a6bb825229bc37b3752d004be8d61fe02353973
alan <alancf@gmail.com>
2019-10-31 15:28:37 +0100

    Added project search filters

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "@material-ui/core/Card";
+import Paper from "@material-ui/core/Paper";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
@@ -13,10 +14,43 @@ import Page from "../../hoc/Page";
 import { colors } from "../../constants/colors";
 
 const Projects = () => {
+  const tags = [];
+  projects.forEach(project => {
+    project.tags.forEach(tag => {
+      if (!tags.find(t => t === tag)) {
+        tags.push(tag);
+      }
+    });
+  });
+
+  const [filteredProjects, setFilteredProjects] = useState(projects);
+
+  const handleClick = clickedTag => {
+    const updatedProjects = projects.filter(project =>
+      project.tags.find(tag => tag === clickedTag)
+    );
+    setFilteredProjects(updatedProjects);
+  };
+
   return (
     <Page>
+      <Grid container spacing={2} className={classes.FilterContainer}>
+        <Grid item xs={12} lg={9}>
+          <Paper className={classes.Filter}>
+            {tags.map(tag => (
+              <Chip
+                label={tag}
+                key={tag}
+                clickable
+                onClick={() => handleClick(tag)}
+                className={classes.Tag}
+              />
+            ))}
+          </Paper>
+        </Grid>
+      </Grid>
       <Grid container spacing={2}>
-        {projects.map(project => (
+        {filteredProjects.map(project => (
           <Grid item xs={12} sm={6} md={6} lg={3} key={project.name}>
             <Card className={classes.Project}>
               <CardContent>
