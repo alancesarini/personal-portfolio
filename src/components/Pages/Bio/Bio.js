290ec069dea5f33dfcf2a88cdf172db2d6b5c168
alancesarini <alancf@gmail.com>
2019-11-04 18:08:48 +0100

    Changed CardContent for Avatar in the Bio page

--- src/components/Pages/Bio/Bio.js
+++ src/components/Pages/Bio/Bio.js
@@ -2,9 +2,9 @@ import React from "react";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
 import classes from "./Bio.module.css";
 import data from "../../../data/bio";
 import Grid from "@material-ui/core/Grid";
@@ -16,33 +16,39 @@ const Bio = props => {
     <Page>
       <PageTitle title="Bio" />
       <Grid container justify="center">
-        <Grid item lg={4} xs={12}>
+        <Grid item lg={6} xs={12}>
           <Card className={classes.Bio}>
-            <CardMedia
-              component="img"
-              alt={data.image.title}
-              height={data.image.height}
-              image={data.image.src}
-              title={data.image.title}
-            />
             <CardContent>
-              <Typography gutterBottom variant="h3" component="h2">
-                {data.text.title}
-              </Typography>
-              <Typography gutterBottom variant="h5" component="h3">
-                {data.text.subtitle}
-              </Typography>
-              {data.text.content.map(paragraph => (
-                <Typography
-                  variant="body1"
-                  color="textSecondary"
-                  component="p"
-                  paragraph
-                  key={paragraph}
-                >
-                  {paragraph}
-                </Typography>
-              ))}
+              <Grid container justify="center">
+                <Grid item lg={2} xs={12}>
+                  <div className={classes.AvatarContainer}>
+                    <Avatar
+                      alt="Alan Cesarini"
+                      src={data.image.src}
+                      className={classes.Avatar}
+                    />
+                  </div>
+                </Grid>
+                <Grid item lg={10} xs={12}>
+                  <Typography gutterBottom variant="h3" component="h2">
+                    {data.text.title}
+                  </Typography>
+                  <Typography gutterBottom variant="h5" component="h3">
+                    {data.text.subtitle}
+                  </Typography>
+                  {data.text.content.map(paragraph => (
+                    <Typography
+                      variant="body1"
+                      color="textSecondary"
+                      component="p"
+                      paragraph
+                      key={paragraph}
+                    >
+                      {paragraph}
+                    </Typography>
+                  ))}
+                </Grid>
+              </Grid>
             </CardContent>
             <CardActions>
               <Button size="small" color="primary">
