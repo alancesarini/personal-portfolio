35f14077d1a67ef2d07471b1349fb97a80147e52
alancesarini <alan@alancesarini.com>
2020-02-28 15:36:41 +0100

    Added a new course to my bio

--- src/data/bio.js
+++ src/data/bio.js
@@ -1,55 +1,60 @@
 const bio = {
   image: {
     height: 350,
-    src: "/assets/images/alancesarini.jpg",
-    title: "Alan Cesarini"
+    src: '/assets/images/alancesarini.jpg',
+    title: 'Alan Cesarini'
   },
   text: {
-    title: "Alan Cesarini",
-    subtitle: "Full stack developer",
+    title: 'Alan Cesarini',
+    subtitle: 'Full stack developer',
     content: [
       "Hi! I'm Alan, a full stack developer with <strong>20 years of experience</strong> developing for the web.",
-      "Although I call myself a <i>full stack developer</i> or a <i>web developer</i>, I consider myself a problem solver, because each project that lands on my hands is just a series of problems/puzzles waiting to be solved. And that is why I love what I do. I have been developing for the web since the year 2000, and in all these years I have learned many programming languages and tools that have helped me to solve all those puzzles. I have worked with <strong>ColdFusion</strong>, <strong>ASP</strong>, <strong>ASPX</strong> and <strong>Java</strong> over the years. In the last 6 years I have been developing <strong>Wordpress</strong> and <strong>WooCommerce</strong> projects (more than 50 projects already).",
-      "In the last few months I have started taking courses and working with <strong>ReactJS</strong>, <strong>NodeJS</strong> and <strong>MongoDB</strong>."
+      'Although I call myself a <i>full stack developer</i> or a <i>web developer</i>, I consider myself a problem solver, because each project that lands on my hands is just a series of problems/puzzles waiting to be solved. And that is why I love what I do. I have been developing for the web since the year 2000, and in all these years I have learned many programming languages and tools that have helped me to solve all those puzzles. I have worked with <strong>ColdFusion</strong>, <strong>ASP</strong>, <strong>ASPX</strong> and <strong>Java</strong> over the years. In the last 6 years I have been developing <strong>Wordpress</strong> and <strong>WooCommerce</strong> projects (more than 50 projects already).',
+      'In the last few months I have started taking courses and working with <strong>ReactJS</strong>, <strong>NodeJS</strong> and <strong>MongoDB</strong>.'
     ]
   },
   social: {
-    linkedin: "https://www.linkedin.com/in/alancesarinifarrow/",
-    github: "https://github.com/alancesarini"
+    linkedin: 'https://www.linkedin.com/in/alancesarinifarrow/',
+    github: 'https://github.com/alancesarini'
   },
   skills: [
-    "ReactJS",
-    "NodeJS",
-    "MongoDB",
-    "Javascript",
-    "jQuery",
-    "Wordpress",
-    "PHP",
-    "MySQL",
-    "HTML5",
-    "CSS3",
-    "Git"
+    'ReactJS',
+    'NodeJS',
+    'MongoDB',
+    'Javascript',
+    'jQuery',
+    'Wordpress',
+    'PHP',
+    'MySQL',
+    'HTML5',
+    'CSS3',
+    'Git'
   ],
   courses: [
     {
-      name: "React - The Complete Guide",
-      link: "https://www.udemy.com/course/react-the-complete-guide-incl-redux/"
+      name: 'React - The Complete Guide',
+      link: 'https://www.udemy.com/course/react-the-complete-guide-incl-redux/'
     },
     {
-      name: "The Complete Node.js Developer Course",
+      name: 'The Complete Node.js Developer Course',
       link:
-        "https://www.udemy.com/course/the-complete-nodejs-developer-course-2/"
+        'https://www.udemy.com/course/the-complete-nodejs-developer-course-2/'
     },
     {
-      name: "React Native - The Practical Guide",
-      link: "https://www.udemy.com/course/react-native-the-practical-guide/"
+      name: 'React Native - The Practical Guide',
+      link: 'https://www.udemy.com/course/react-native-the-practical-guide/'
     },
     {
-      name: "JavaScript - The Complete Guide 2020",
+      name: 'JavaScript - The Complete Guide 2020',
       link:
-        "https://www.udemy.com/course/javascript-the-complete-guide-2020-beginner-advanced/"
+        'https://www.udemy.com/course/javascript-the-complete-guide-2020-beginner-advanced/'
+    },
+    {
+      name: 'The React Bootcamp',
+      link:
+        'https://scrimba.com/g/greact'
     }
   ]
-};
+}
 
-export default bio;
+export default bio
