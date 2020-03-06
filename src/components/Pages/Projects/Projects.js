13006df7524377d0c4a968b1e3839a5ee76f1447
alancesarini <alan@alancesarini.com>
2020-03-06 18:55:14 +0100

    New project and "demo" button in the project card

--- src/components/Pages/Projects/Projects.js
+++ src/components/Pages/Projects/Projects.js
@@ -1,45 +1,46 @@
-import React, { useState } from "react";
-import Card from "@material-ui/core/Card";
-import Paper from "@material-ui/core/Paper";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import Grid from "@material-ui/core/Grid";
-import Chip from "@material-ui/core/Chip";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import classes from "./Projects.module.css";
-import projects from "../../../data/projects";
-import Page from "../../hoc/Page";
-import PageTitle from "../../UI/PageTitle/PageTitle";
+import React, { useState } from 'react'
+import Card from '@material-ui/core/Card'
+import Paper from '@material-ui/core/Paper'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+import Chip from '@material-ui/core/Chip'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import ExitToApp from '@material-ui/icons/ExitToApp'
+import classes from './Projects.module.css'
+import projects from '../../../data/projects'
+import Page from '../../hoc/Page'
+import PageTitle from '../../UI/PageTitle/PageTitle'
 
 const Projects = () => {
-  const tags = [];
+  const tags = []
   projects.forEach(project => {
     project.tags.forEach(tag => {
       if (!tags.find(t => t === tag)) {
-        tags.push(tag);
+        tags.push(tag)
       }
-    });
-  });
+    })
+  })
 
-  const [filteredProjects, setFilteredProjects] = useState(projects);
+  const [filteredProjects, setFilteredProjects] = useState(projects)
 
   const handleClick = clickedTag => {
     const updatedProjects = projects.filter(project =>
       project.tags.find(tag => tag === clickedTag)
-    );
-    setFilteredProjects(updatedProjects);
-  };
+    )
+    setFilteredProjects(updatedProjects)
+  }
 
   return (
     <Page>
-      <PageTitle title="My projects" />
+      <PageTitle title='My projects' />
       <Grid
         container
         spacing={2}
         className={classes.FilterContainer}
-        justify="center"
+        justify='center'
       >
         <Grid item xs={12} lg={9}>
           <Paper className={classes.Filter}>
@@ -55,18 +56,18 @@ const Projects = () => {
           </Paper>
         </Grid>
       </Grid>
-      <Grid container spacing={2} justify="center">
+      <Grid container spacing={2} justify='center'>
         {filteredProjects.map(project => (
           <Grid item xs={12} sm={6} md={6} lg={3} key={project.name}>
             <Card className={classes.Project}>
               <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
+                <Typography gutterBottom variant='h5' component='h2'>
                   {project.name}
                 </Typography>
                 <Typography
-                  variant="body2"
-                  color="textSecondary"
-                  component="p"
+                  variant='body2'
+                  color='textSecondary'
+                  component='p'
                   paragraph
                 >
                   {project.description}
@@ -74,17 +75,26 @@ const Projects = () => {
                 {project.tags.map(tag => (
                   <Chip
                     className={classes.Tag}
-                    size="small"
+                    size='small'
                     label={tag}
                     key={tag}
                   />
                 ))}
               </CardContent>
               <CardActions className={classes.ProjectActions}>
+                {project.demo ? <Button
+                  variant='contained'
+                  color='primary'
+                  size='small'
+                  startIcon={<ExitToApp />}
+                  href={project.demo}
+                                >
+                  DEMO
+                                </Button> : null}
                 <Button
-                  variant="contained"
-                  color="primary"
-                  size="small"
+                  variant='contained'
+                  color='primary'
+                  size='small'
                   startIcon={<GitHubIcon />}
                   href={project.link}
                 >
@@ -96,7 +106,7 @@ const Projects = () => {
         ))}
       </Grid>
     </Page>
-  );
-};
+  )
+}
 
-export default Projects;
+export default Projects
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,25 +1,36 @@
 const projects = [
   {
-    name: "Survey generator",
+    name: 'Contagion',
     description:
-      "This is a basic project to create and manage surveys. Each survey has its own unique url so that users can access and fill it.",
-    tags: ["ReactJS", "NodeJS", "Express", "MongoDB"],
-    link: "https://github.com/alancesarini/survey-system"
+      'A simple ReactJS dashboard with info on a (simulated) pandemia.',
+    tags: ['ReactJS'],
+    link: 'https://github.com/alancesarini/contagion',
+    demo: 'https://demo.alancesarini.com/contagion/'
   },
   {
-    name: "Image search engine",
+    name: 'Survey generator',
     description:
-      "An image search engine made with ReactJS. It uses the Unsplash API to search through its image database.",
-    tags: ["ReactJS"],
-    link: "https://github.com/alancesarini/unsplash-search-engine"
+      'This is a basic project to create and manage surveys. Each survey has its own unique url so that users can access and fill it.',
+    tags: ['ReactJS', 'NodeJS', 'Express', 'MongoDB'],
+    link: 'https://github.com/alancesarini/survey-system',
+    demo: ''
   },
   {
-    name: "Personal portfolio",
+    name: 'Image search engine',
     description:
-      "A portfolio website made with ReactJS. The website you are browsing is based on this project.",
-    tags: ["ReactJS"],
-    link: "https://github.com/alancesarini/personal-portfolio"
+      'An image search engine made with ReactJS. It uses the Unsplash API to search through its image database.',
+    tags: ['ReactJS'],
+    link: 'https://github.com/alancesarini/unsplash-search-engine',
+    demo: ''
+  },
+  {
+    name: 'Personal portfolio',
+    description:
+      'A portfolio website made with ReactJS. The website you are browsing is based on this project.',
+    tags: ['ReactJS'],
+    link: 'https://github.com/alancesarini/personal-portfolio',
+    demo: ''
   }
-];
+]
 
-export default projects;
+export default projects
